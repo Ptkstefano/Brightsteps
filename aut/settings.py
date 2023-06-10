b667a3506e4693f69e2e339200cec864e9d3fd12
PtkStefano <patrick.stefano.pereira@gmail.com>
2023-06-10 18:06:21 -0300

    a

--- aut/settings.py
+++ aut/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path, os
-from env import load_env
+from env import load_dotenv
 
-load_env()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
