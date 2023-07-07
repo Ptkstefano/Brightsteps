4653b9926eb2a2c61b2f913abb092eb637c77a78
Ptkstefano <johndoe@example.com>
2023-07-07 16:43:41 -0300

    a

--- aut/settings.py
+++ aut/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'aut.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'T7rMFZI7yZU2qeQEc8ig',
+        'HOST': 'containers-us-west-17.railway.app',
+        'PORT': '6655',
     }
 }
 
